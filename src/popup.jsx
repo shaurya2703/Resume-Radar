9140d76e40bf68b02b9ff7d83b0d3dfb026f904c
Shaurya Khanna <shauryakhanna56@gmail.com>
2023-11-01 03:04:01 +0530

    Added some style

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
 const Popup = () => {
+  const [resumeFile, setResumeFile] = useState(null);
+  const [jobDescription, setJobDescription] = useState('');
+
+  const checkMatch = () => {
+    // Logic to check matching between resume and job description
+    console.log("Checking match...");
+    // For now, just log if a file is uploaded and the job description
+    console.log(resumeFile, jobDescription);
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setResumeFile(file);
+    }
+  };
+
   return (
     <div>
       <h1>Resume Radar</h1>
-      <p>Check your match with the current job!</p>
+      <input
+        type="file"
+        onChange={handleFileChange}
+      />
+      <textarea
+        placeholder="Paste the job description here"
+        value={jobDescription}
+        onChange={e => setJobDescription(e.target.value)}
+      ></textarea>
+      <button onClick={checkMatch}>Check Match</button>
     </div>
   );
 };
 
-ReactDOM.render(<Popup />, document.getElementById('react-target'));
+ReactDOM.render(<Popup />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,13 @@ module.exports = {
         options:{
             presets: [ '@babel/preset-env','@babel/preset-react']
         }
+    },
+    },
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
     }
-    }],
+  ],
   },
   plugins: [
     new HtmlWebpackPlugin({
